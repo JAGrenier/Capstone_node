14d33403a4269657ba33de9d9033ee0d35d5e97a
github username <github email address>
2020-10-27 11:45:45 -0600

    update route

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express = require("express")
-
+const morgan = require('morgan') 
 const app = express()
+
+app.use(express.json())
+
 // get all restaurants
 app.get("/api/v1/restaurants", (req, res) => {
+    console.log("route handler hit")
     res.status(200).json({
         status: "success",
         data:{
@@ -13,16 +17,40 @@ app.get("/api/v1/restaurants", (req, res) => {
     })
 })
 
-// get individual restaurants
+// get a restaurant
 app.get("/api/v1/restaurants/:id", (req, res) => {
-    console.log(req)
+    console.log(req. params)
+
+    res.status(200).json()({
+        status: "success",
+        data: {
+            restaurant: ["Jenn's Bagels"]
+        }
+    })
 })
 
 // create a restaurant 
 app.post("/api/v1/restaurants", (req, res) => {
-    console.log(req)
-})
+    console.log(req.body);
+    res.status(201).json()({
+        status: "success",
+        data: {
+            restaurant: ["AJ's Bagels"]
+        }
+    })
+});
 
+// update a restaurant 
+app.put("/api/v1/restaurants/:id", (req, res) =>{
+    console.log(req.params.id)
+    console.log(req.body)
+    res.status(200).json()({
+        status: "success",
+        data: {
+            restaurant: ["Jackiess Bagels"]
+        }
+    })
+})
 
 const port = process.env.PORT || 9000;
 
