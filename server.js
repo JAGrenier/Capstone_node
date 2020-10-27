7c0cd50716af34c528af34547ad95b093f5c7e0e
github username <github email address>
2020-10-27 11:11:24 -0600

    show express route

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ require("dotenv").config();
 const express = require("express")
 
 const app = express()
-
-app.get("/getRestaurants", (req, res) => {
-    res.json({
-        status: "sucess",
-        restaurant: "mcyds"
+// get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data:{
+            restaurant: ["mcyds", "Jenn's Bagels"]
+        }
+        
     })
 })
+
+// get individual restaurants
+app.get("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req)
+})
+
 
 const port = process.env.PORT || 9000;
 
 app.listen(port, () => {
     console.log(`server is up and on port ${port}`)
-})
+});
 
