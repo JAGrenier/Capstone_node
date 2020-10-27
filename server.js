a82b2bcccd5c0d38b31fd851d012471531a86ed9
github username <github email address>
2020-10-27 10:35:46 -0600

    first express route

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require("express")
 
 const app = express()
 
+app.get("/getRestaurants", (req, res) => {
+    res.json({
+        status: "sucess",
+        restaurant: "mcyds"
+    })
+})
+
 const port = process.env.PORT || 9000;
 
 app.listen(port, () => {
