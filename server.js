d29d5d14b03facf49043f05b0e309e38e061016e
github username <github email address>
2020-11-04 13:00:42 -0700

    switch sql commands to new db

--- server.js
+++ server.js
@@ -29,12 +29,13 @@ app.get("/api/v1/restaurants", async(req, res) => {
 });
 
 // get a restaurant
-app.get("/api/v1/restaurants/:id", async (req, res) => {
+app.get(
+    "/api/v1/restaurants/:id", 
+    async (req, res) => {
     console.log(req.params.id)
     try{
-        const restaurant = await db.query('select * from restaurants where id = $1', [
-            req.params.id,
-        ]);
+        const restaurant = await db.query('SELECT * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id where id = $1',
+        [req.params.id,]);
         const reviews = await db.query('select * from reviews where restaurant_id = $1', [
             req.params.id,
         ]);
@@ -56,7 +57,7 @@ app.post("/api/v1/restaurants", async (req, res) => {
     console.log(req.body);
 
     try{
-        const results = await db.query("INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *", [req.body.name, req.body.location, req.body.price_range])
+        const results = await db.query("INSERT INTO restaurants (name, lat, lng, image) values ($1, $2, $3, $4) returning *", [req.body.name, req.body.lat, req.body.lng, req.body.image])
         console.log(results)
         res.status(201).json({
         status: "success",
@@ -69,7 +70,7 @@ app.post("/api/v1/restaurants", async (req, res) => {
     }
 });
 
-// update a restaurant 
+// update a restaurant not currently implemented 
 app.put("/api/v1/restaurants/:id", async (req, res) =>{
     try{
         const results = await db.query('UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *', 
@@ -87,7 +88,7 @@ app.put("/api/v1/restaurants/:id", async (req, res) =>{
     console.log(req.params.id)
     console.log(req.body)
 })
-// delete a restaurant 
+// delete a restaurant not currently implemented 
 app.delete("/api/v1/restaurants/:id", async (req, res) =>{
     try{
         const results = await db.query("DELETE FROM restaurants where id = $1", 
@@ -104,8 +105,8 @@ app.delete("/api/v1/restaurants/:id", async (req, res) =>{
 app.post("/api/v1/restaurants/:id/addReview", async(req, res) => {
     try{
         const newReview = await db.query(
-            "INSERT INTO reviews (restaurant_id, name, review, rating) values ($1, $2, $3, $4) returning *;" , 
-        [req.params.id, req.body.name, req.body.review, req.body.rating])
+            "INSERT INTO reviews (restaurant_id, name, disability, rating, review, image) values ($1, $2, $3, $4, $5, $6) returning *;" , 
+        [req.params.id, req.body.name, req.body.disability, req.body.rating, req.body.review, req.body.image])
         console.log(newReview)
         res.status(201).json({
             status: 'success',
